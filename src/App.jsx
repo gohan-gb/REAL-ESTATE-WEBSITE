765a59e4228e55f28d339b8b4ab5af3dee6c6578
Gobinda B <gobinda2001b@gmail.com>
2024-12-05 12:18:57 +0530

    basic routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import Container from "./components/Container"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/home/Home"
+import Property from "./pages/property/Property"
+import Contact from "./pages/contact/Contact"
 
 function App() {
 
 
   return (
     <>
-    <Container>
+
+    <Navbar />
+    <main>
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="/contact" element={<Property/>} />
+        <Route path="/property" element={<Contact/>} />
+      </Routes>
+    </main>
+    <Footer />
+
+    
+    {/* <Container>
     <div className= "text-4xl "></div>
      <h1 className="h1 "> hello world </h1>
 
@@ -17,7 +35,7 @@ function App() {
      <h1 className="heading "> hello world </h1>
      <h1 className="headingTwo"> hello world </h1>
      <h1 className=""> hello world </h1>
-    </Container>
+    </Container> */}
      
     </>
   )
